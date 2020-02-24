f0c251d1a0db8549afe78dc95804a3060bb645a7
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-02-24 18:52:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,19 @@
 class Dog 
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  def initialize(breed)
+    @breed = name
+  end
+  
+  def breed
+    @breed
+  end
+  
 end
